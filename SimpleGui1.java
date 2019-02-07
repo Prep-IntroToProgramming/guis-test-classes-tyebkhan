7b917ea3c71ba4a75a5f0a72c84a94d62ec1c357
Daniel Tyebkhan <19TyebkhanD@flintridgeprep.org>
2019-02-07 09:23:02 -0800

    messed around

--- SimpleGui1.java
+++ SimpleGui1.java
@@ -2,15 +2,15 @@ import javax.swing.*;
 
 public class SimpleGui1{ 
     public SimpleGui1() {
-        JFrame frame = new JFrame();
-        JButton button = new JButton("click me");
+        JFrame frame = new JFrame();//creates a new frame
+        JButton button = new JButton("click me");//creates a button that says click me
         //JLabel label = new JLabel("Swing is pretty cool");
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(button);
+        frame.add(button);//puts the button in the frame object
         //frame.add(label);
         frame.setSize(300, 300);
-        frame.setVisible(true);
+        frame.setVisible(true);//makes the window visible
     }
 
     public static void main(String[] args) {
--- SimpleGui1B.java
+++ SimpleGui1B.java
@@ -4,27 +4,39 @@ import java.awt.event.*;
 public class SimpleGui1B implements ActionListener{ 
     JFrame frame;
     JButton button;
-    
+    int clicks;
     public SimpleGui1B() {
-        button = new JButton("click me");
+        button = new JButton("don't click me");
         frame = new JFrame();
-
+        clicks = 0;
         //JLabel label = new JLabel("Swing is pretty cool");
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(button);
         //frame.add(label);
-        
-        button.addActionListener(this);
         frame.setSize(300, 300);
         frame.setVisible(true);
+
+        button.addActionListener(this);//waits for the button to be interacted with
+
     }
 
     public void actionPerformed(ActionEvent event) {
-        button.setText("I've been clicked!");
+        clicks++;
+        if(button.getText().equals("don't click me")){
+            button.setText("Why don't you listen?");
+        }else{
+            button.setText("don't click me");
+        }
+        if(clicks%5 == 0){
+            System.out.println(clicks + " is a multiple of 5");
+        }else{
+            System.out.println(clicks);
+        }
     }
-    
+
     public static void main(String[] args) {
         SimpleGui1B gui = new SimpleGui1B();
     }
+
 } 
