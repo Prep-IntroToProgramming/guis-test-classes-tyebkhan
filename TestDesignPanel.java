e74ca66bf3d847f0b99529327cd9268c6f97f431
Daniel Tyebkhan <19TyebkhanD@flintridgeprep.org>
2019-02-22 08:53:11 -0800

    bouncing ball

--- TestDesignPanel.java
+++ TestDesignPanel.java
@@ -1,39 +1,53 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 public class TestDesignPanel{
     JFrame frame;
     Boolean move = true;
-    Boolean forward = true;
+    Boolean xdirection = true;
+    Boolean ydirection = true;
     DrawDesignPanel ball = new DrawDesignPanel();
+    Timer myTimer;
     public TestDesignPanel(){
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(ball);
         frame.setSize(500,500);
         frame.setVisible(true);
-        while(move == true){ 
-            if(ball.x==frame.getWidth()&&ball.y==frame.getHeight()){
-                forward = false;
+        myTimer = new Timer(4, new TimerListener()); 
+        myTimer.start();
+    }
+    public class TimerListener implements ActionListener{
+        public void actionPerformed(ActionEvent a){
+
+            if(ball.x==frame.getWidth()-120){
+                xdirection = false;
             }
-            if(ball.x==0&&ball.y==0){
-                forward = true;
+            if(ball.x==0){
+                xdirection = true;
             }
-            if(forward==true){
+            if(xdirection==true){
                 ball.x++;
-                ball.y++;
             }else{
                 ball.x--;
+            }
+            if(ball.y==frame.getHeight()-125){
+                ydirection = false;
+            }
+            if(ball.y==0){
+                ydirection = true;
+            }
+            if(ydirection==true){
+                ball.y++;
+            }else{
                 ball.y--;
             }
-            
+
             frame.repaint();
             //System.out.println(ball.x + "," + ball.y);
-            try{
-                Thread.sleep(10);
-            }catch(InterruptedException e){
 
-            }
         }
+
     }
 
     public static void main(String[] args){
